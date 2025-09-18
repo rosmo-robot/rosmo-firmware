9a1dd4fe9dc9ab9f9a30f4d16f9ff0faa88e112d
samuk <sam@bristolwireless.net>
2025-09-18 08:57:11 +0100

    Update rosmo.cpp

--- firmware/src/rosmo.cpp
+++ firmware/src/rosmo.cpp
@@ -3,3 +3,14 @@ ROSMO robot firmware
 */
 
 #include "config.h"
+
+void setup() {
+  // put your initialization code here, e.g. pinMode(...)
+}
+
+void loop() {
+  // put your main code here, e.g. motor control, sensor reading, etc.
+}
+
+
+
